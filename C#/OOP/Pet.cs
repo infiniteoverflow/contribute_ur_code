07467ff6244ed172fc2815b8fa32e6ee75bfce69
dvir <dvir019@gmail.com>
2019-10-13 18:16:53 +0300

    Add oop example

---
+++ C#/OOP/Pet.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hacktoberfest
+{
+    // Inheritance
+    public abstract class Pet : Animal
+    {
+        private string owner;
+
+        protected Pet(string name, int age, string owner) : base(name, age)
+        {
+            this.owner = owner;
+        }
+
+        public string Owner
+        {
+            get { return owner; }
+            set { owner = value; }
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0}\nOwner: {1}", base.ToString(), owner);
+        }
+    }
+}
