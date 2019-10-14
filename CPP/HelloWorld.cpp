2f043fd6d2811e337796734efaa032ff3111d80c
Abhishek jain <abhijain03102000@gmail.com>
2019-10-14 15:50:14 +0530

    Hacktoberfest2019 Abhishek jain

--- CPP/HelloWorld.cpp
+++ CPP/HelloWorld.cpp
@@ -1,6 +1,7 @@
-#include <bits/stdc++.h>
-using namespace std;
-int main() {
+#include <iostream>
+using namespace std;  //This is the most basic program in cpp
+int main() 
+{
     cout << "Hello, World!" << endl ;
     return 0;
 }
