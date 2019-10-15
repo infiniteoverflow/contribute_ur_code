ab16bee4b6289f38769d84a42e03fa16ea11355b
William Ardianto <william.ardianto@gmail.com>
2019-10-15 10:42:15 +0800

    Create calculator.sh

---
+++ Bash/calculator.sh
@@ -0,0 +1,24 @@
+!/bin/bash 
+
+echo "input two numbera: "
+read a
+read b
+
+echo "choose operation: "
+echo "1. Addition"
+echo "2. Subtraction"
+echo "3. Multiplication"
+echo "4. Division"
+read choice
+
+case $choice in
+  1)res=`echo $a + $b | bc` 
+  ;; 
+  2)res=`echo $a - $b | bc` 
+  ;; 
+  3)res=`echo $a \* $b | bc` 
+  ;; 
+  4)res=`echo "scale=2; $a / $b" | bc` 
+  ;; 
+esac
+echo "Result : $res"
