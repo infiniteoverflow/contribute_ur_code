0945a473ab65735b9845fc390e6952a6a727c253
dror1212 <drortal12@gmail.com>
2019-10-18 13:14:54 +0300

    Change the folder name to more correctly one

---
+++ Assembly 8086/hello_world.asm
@@ -0,0 +1,29 @@
+; multi-segment executable file template.
+
+data segment
+    ; add your data here!
+    pkey db "Hello world$"
+ends
+
+stack segment
+    dw   128  dup(0)
+ends
+
+code segment
+start:
+; set segment registers:
+    mov ax, data
+    mov ds, ax
+    mov es, ax
+
+    ; add your code here
+            
+    lea dx, pkey
+    mov ah, 9
+    int 21h        ; output string at ds:dx
+    
+    mov ax, 4c00h ; exit to operating system.
+    int 21h    
+ends
+
+end start ; set entry point and stop the assembler.
