5518b1d1e04bcc33e06150e797dc87324fb523c1
Mukheem Mohammed <49473863+Mukheem1603@users.noreply.github.com>
2019-10-25 20:29:12 +0530

    Update Mario_stairs.c

--- C/Mario_stairs.c
+++ C/Mario_stairs.c
@@ -1,11 +1,15 @@
-#include <studio.h>
+#include <stdio.h>
 int main()
 {
-    for(int i = 0; i < 5; i++)
+    int i,j,n;
+    printf("Enter the number of floors:");
+    scanf("%d",&n);
+    for(i = 0; i < n; i++)
     {
-        for(int j = 0; j < i; j++)
+        for(j = 0; j <=i; j++)
         {
             printf("#");
         }
+        printf("\n");
     }
 }
